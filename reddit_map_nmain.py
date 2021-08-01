f0f111fbbab8637ad7cf5ca4830025bf35691afa
San Ha Kim <sanhakim1993@gmail.com>
2021-08-02 00:03:41 +0900

    Updated code for list comprehension

--- reddit_map_nmain.py
+++ reddit_map_nmain.py
@@ -17,22 +17,23 @@ import matplotlib.pyplot as plt
 # ===== Create class object for posts =====
 #class RedditSub:"""
 
+# ===== Instantiate Reddit instance =====
+reddit = praw.Reddit(
+    client_id='FE5apSBviVjlEQ',
+    client_secret='4lOv7xDgIn-XzFBmyFeBB8REpkTQzQ',
+    username='mrkillercow',
+    password='S4nh4k1mi99e',
+    user_agent='1'
+)
+
 
 def get_posts(num=300, sub='conspiracy'):
     """
     This function retrieves a certain number of posts from a specified Subreddit,
     using specified sorts (e.g. hot, top, new). It returns the post ID, poster's username, post time, number of up/down
     votes.
-    :return:
-
+    :return: Hot posts of specified subreddit
     """
-    reddit = praw.Reddit(
-        client_id='FE5apSBviVjlEQ',
-        client_secret='4lOv7xDgIn-XzFBmyFeBB8REpkTQzQ',
-        username='mrkillercow',
-        password='S4nh4k1mi99e',
-        user_agent='1'
-    )
 
     sub = reddit.subreddit(sub)
 
@@ -46,4 +47,11 @@ r = get_posts(num=5, sub='TwoXChromosomes')
 # --- Obtain the most active posters of a subreddit ---
 users = [s.author for s in r if not s.stickied]
 
-print(*users)
+# --- Iterate over the users to see each user's most recent posts ---
+for user in users:
+    posts = user.submissions.new(limit=7)
+
+    for p in posts:
+        print(user)
+        print(reddit.submission(id=p).subreddit)
+        print(20*'-')
